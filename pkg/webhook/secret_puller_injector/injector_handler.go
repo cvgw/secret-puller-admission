5819c9b3eb63b2a452b4cc456c6ae7e67e66b6c8
Cole Wippern <cgwippern@gmail.com>
2018-12-23 14:16:58 -0800

    webhook skeleton added

---
+++ pkg/webhook/secret_puller_injector/injector_handler.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2018 Cole Wippern.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package secret_puller_injector
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws/client"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/types"
+)
+
+type secretPullerInjector struct {
+	client  client.Client
+	decoder types.Decoder
+}
+
+// secretPullerInjector implements admission.Handler.
+var _ admission.Handler = &secretPullerInjector{}
+
+// Automatically generate RBAC rules to allow the Controller to read and write Deployments
+// +kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=,resources=services,verbs=get;list;watch;create;update;patch;delete
+func (a *secretPullerInjector) Handle(ctx context.Context, req types.Request) types.Response {
+	pod := &corev1.Pod{}
+
+	err := a.decoder.Decode(req, pod)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusBadRequest, err)
+	}
+	copy := pod.DeepCopy()
+
+	err = a.mutatePodsFn(ctx, copy)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusInternalServerError, err)
+	}
+
+	// admission.PatchResponse generates a Response containing patches.
+	return admission.PatchResponse(pod, copy)
+}
+
+// secretPullerInjector implements inject.Client.
+var _ inject.Client = &secretPullerInjector{}
+
+// InjectClient injects the client into the secretPullerInjector
+func (a *secretPullerInjector) InjectClient(c client.Client) error {
+	a.client = c
+	return nil
+}
+
+// secretPullerInjector implements inject.Decoder.
+var _ inject.Decoder = &secretPullerInjector{}
+
+// InjectDecoder injects the decoder into the secretPullerInjector
+func (a *secretPullerInjector) InjectDecoder(d types.Decoder) error {
+	a.decoder = d
+	return nil
+}
+
+func (a *secretPullerInjector) mutatePodsFn(ctx context.Context, copy *corev1.Pod) error {
+	log.Println("I got a request")
+
+	return nil
+}
